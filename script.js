e292b04764ea377075c58032b4bbde72e170df60
Evie Edwards <evieedwards@mac.com>
2024-11-15 15:51:00 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('about-link').addEventListener('click', function(e) {
         e.preventDefault();
         const aboutText = document.getElementById('about-text');
-        aboutText.style.display = aboutText.style.display === 'none' ? 'block' : 'none';
+        aboutText.classList.toggle('visible');
     });
-}); 
+});
